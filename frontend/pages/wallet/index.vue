c6e547934c02d3166786d9574ba6e1c599fb7299
alvintanoto <alvintanotodev@gmail.com>
2023-09-17 01:05:48 +0700

    add wallet form and change wallet item list

--- frontend/pages/wallet/index.vue
+++ frontend/pages/wallet/index.vue
@@ -1,34 +1,46 @@
 <script setup>
 const layout = "client";
 
-const colors = [
-  "bg-rp-dawn-love/10 dark:bg-rp-moon-love/10 text-rp-dawn-love dark:text-rp-moon-love",
-  "bg-rp-dawn-gold/10 dark:bg-rp-moon-gold/10 text-rp-dawn-gold dark:text-rp-moon-gold",
-  "bg-rp-dawn-rose/10 dark:bg-rp-moon-rose/10 text-rp-dawn-rose dark:text-rp-moon-rose",
-  "bg-rp-dawn-pine/10 dark:bg-rp-moon-pine/10 text-rp-dawn-pine dark:text-rp-moon-pine",
-  "bg-rp-dawn-foam/10 dark:bg-rp-moon-foam/10 text-rp-dawn-foam dark:text-rp-moon-foam",
-  "bg-rp-dawn-iris/10 dark:bg-rp-moon-iris/10 text-rp-dawn-iris dark:text-rp-moon-iris",
+const wallets = [
+  {
+    name: "Kantong USD",
+    currency: "USD",
+    createdAt: "2 September 2023",
+  },
 ];
 </script>
 
 <template>
   <NuxtLayout :name="layout">
-    <div
-      class="w-full rounded-lg px-4"
-    >
-      <div class="text-xl p-4 my-2">Wallet Management</div>
-      <button class="mx-2 mb-4 px-4 py-2 bg-rp-dawn-iris/20 dark:bg-rp-moon-pine/80 hover:bg-rp-dawn-iris/40 hover:dark:bg-rp-moon-pine rounded-md">Create Wallet</button>
+    <div class="w-full rounded-lg px-4">
+      <div class="text-xl m-2 my-4">Wallet Management</div>
+      <nuxt-link :to="{ path: '/wallet/form', query: { action: 'create' } }">
+        <button
+          class="mx-2 mb-4 px-4 py-2 bg-rp-dawn-iris/20 dark:bg-rp-moon-pine/80 hover:bg-rp-dawn-iris/40 hover:dark:bg-rp-moon-pine rounded-md"
+        >
+          Create Wallet
+        </button>
+      </nuxt-link>
       <!-- Loop this for list of wallet -->
       <div class="flex flex-row flex-wrap">
         <div
-          v-for="(item, index) in colors"
+          v-for="(item, index) in wallets"
           :key="index"
-          class="p-4 m-2 w-[280px] rounded-md"
-          :class=item
+          class="p-4 m-2 w-[320px] rounded-md cursor-pointer bg-rp-dawn-subtle/10 dark:bg-rp-moon-subtle/10 text-rp-dawn-subtle dark:text-rp-moon-subtle"
+          :class="item"
         >
-          <div class="font-bold">Wallet Name</div>
-          <div class="font-light">USD</div>
-          <div class="text-sm text-right">2 September 2023</div>
+          <nuxt-link
+            :to="{
+              path: '/wallet/form',
+              query: { action: 'update', wallet_id: index },
+            }"
+          >
+            <div class="font-bold min-h-[24px]">{{ item.name }}</div>
+            <div class="font-light min-h-[24px] mb-8">{{ item.currency }}</div>
+            <div class="text-sm text-right min-h-[24px] italic">
+              {{ item.createdAt }}
+            </div>
+          </nuxt-link>
         </div>
       </div>
     </div>
